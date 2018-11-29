368ba320e3dbf95498bd69af0fc8c330c035ff5d
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2018-11-29 19:34:34 +0100

    hop

--- setup.sh
+++ setup.sh
@@ -5,11 +5,15 @@ cat ps1 >> ~/.bashrc
 # Install other dotfiles
 cp vimrc ~/.vimrc
 
+check_for () {
+  command -v $1 >/dev/null
+}
+
 # Move to home dir (to clone all repos there)
 cd ~
 
 # Install stern to view logs
-[ -x /usr/local/bin/stern ] ||
+check_for stern ||
   sudo wget -O /usr/local/bin/stern \
     https://github.com/wercker/stern/releases/download/1.10.0/stern_linux_amd64
 [ -f /etc/bash_completion.d/stern ] ||
@@ -18,9 +22,9 @@ cd ~
 # Install kubectx because it's awesome
 [ -d kubectx ] ||
   git clone https://github.com/ahmetb/kubectx
-[ -f /usr/local/bin/kctx ] ||
+check_for kctx ||
   sudo ln -s ~/kubectx/kubectx /usr/local/bin/kctx
-[ -f /usr/local/bin/kns ] ||
+check_for kns ||
   sudo ln -s ~/kubectx/kubens /usr/local/bin/kns
 [ -f /etc/bash_completion.d/kubectx.bash ] ||
   sudo ln -s ~/kubectx/completion/kubectx.bash /etc/bash_completion.d/kubectx.bash
@@ -30,6 +34,8 @@ cd ~
 # Install kube-ps1 because why not
 [ -d kube-ps1 ] ||
   git clone https://github.com/jonmosco/kube-ps1
+check_for kube-ps1 ||
+  sudo ln -s ~/kube-ps1/kube-ps1 /usr/local/bin/kube-ps1
 
 # Install completion for kubectl
 [ -f /etc/bash_completion.d/kubectl ] ||
